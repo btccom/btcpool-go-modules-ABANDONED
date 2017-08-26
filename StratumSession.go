3a3dacd4d9130f38d113b686d18b49875b7f6156
YihaoPeng <yihao.peng@bitmain.com>
2017-08-26 16:04:08 +0800

    可识别矿机的用户名和子账户名

--- StratumSession.go
+++ StratumSession.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
 	"errors"
 	"net"
+	"strings"
 	"time"
 
 	"github.com/golang/glog"
@@ -21,6 +25,9 @@ type StratumSession struct {
 
 	fullWorkerName string
 	subaccountName string
+
+	stratumSubscribeRequest *JSONRPCRequest
+	stratumAuthorizeRequest *JSONRPCRequest
 }
 
 // sessionIDManager 会话ID管理器实例
@@ -48,6 +55,12 @@ func NewStratumSession(clientConn net.Conn) (*StratumSession, error) {
 	}
 
 	session.sessionID = sessionID
+
+	// uint32 to string
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, sessionID)
+	session.sessionIDString = hex.EncodeToString(bytesBuffer.Bytes())
+
 	glog.Info("Session ID: ", sessionID)
 
 	return session, nil
@@ -96,9 +109,8 @@ func (session *StratumSession) stratumFindWorkerName() {
 	go func() {
 		isSubscribed := false
 		response := new(JSONRPCResponse)
-		running := true
 
-		for running {
+		for true {
 			requestJSON, err := session.clientReader.ReadBytes('\n')
 
 			if err != nil {
@@ -118,30 +130,66 @@ func (session *StratumSession) stratumFindWorkerName() {
 			response.Result = nil
 			response.Error = nil
 
-			switch request.Method {
-			case "mining.subscribe":
+			if request.Method == "mining.subscribe" {
 				if isSubscribed {
 					response.Result = nil
 					response.Error = JSONRPCArray{24, "Duplicate Subscribed", nil}
 				} else {
 					isSubscribed = true
+					// 保存原始请求以便转发给Stratum服务器
+					session.stratumSubscribeRequest = request
 
-					response.Result = "hello"
+					response.Result = JSONRPCArray{JSONRPCArray{JSONRPCArray{"mining.set_difficulty", session.sessionIDString}, JSONRPCArray{"mining.notify", session.sessionIDString}}, session.sessionIDString, 8}
 					response.Error = nil
 				}
-			case "mining.authorize":
+			} else if request.Method == "mining.authorize" {
 				if isSubscribed {
-					response.Result = "ok"
-					response.Error = nil
+					if len(request.Params) >= 1 {
+						fullWorkerName, ok := request.Params[0].(string)
 
-					e <- nil
-					running = false
+						if ok {
+							// 矿工名
+							session.fullWorkerName = strings.TrimSpace(fullWorkerName)
+
+							if strings.Contains(session.fullWorkerName, ".") {
+								// 截取“.”之前的做为子账户名
+								session.subaccountName = session.fullWorkerName[0:strings.Index(session.fullWorkerName, ".")]
+							} else {
+								session.subaccountName = session.fullWorkerName
+							}
+
+							if len(session.subaccountName) >= 1 {
+
+								glog.Info(session.fullWorkerName, " ", session.subaccountName)
+
+								// 保存原始请求以便转发给Stratum服务器
+								session.stratumAuthorizeRequest = request
+
+								// 发送一个空错误到channel，表示成功
+								e <- nil
+								// 跳出循环，不发送响应给矿机，该响应直接由Stratum服务器发送
+								break
+
+							} else {
+								response.Result = nil
+								response.Error = JSONRPCArray{23, "Worker Name Cannot Start with '.'", nil}
+							}
+
+						} else {
+							response.Result = nil
+							response.Error = JSONRPCArray{23, "Worker Name is Not a String", nil}
+						}
+
+					} else {
+						response.Result = nil
+						response.Error = JSONRPCArray{22, "Too Few Params", nil}
+					}
 
 				} else {
 					response.Result = nil
-					response.Error = JSONRPCArray{25, "Not subscribed", nil}
+					response.Error = JSONRPCArray{25, "Not Subscribed", nil}
 				}
-			default:
+			} else {
 				response.Result = nil
 				response.Error = JSONRPCArray{20, "Unknown Method", nil}
 			}
@@ -158,12 +206,17 @@ func (session *StratumSession) stratumFindWorkerName() {
 			glog.Warning(err)
 			session.Stop()
 		} else {
-			glog.Warning("auth success")
+			glog.Info("FindWorkerName Success: ", session.fullWorkerName)
+			session.connectStratumServer()
 		}
 	case <-time.After(90 * time.Second):
 		glog.Warning("FindWorkerName Timeout")
 		session.Stop()
 	}
+}
+
+func (session *StratumSession) connectStratumServer() {
+
 }
 
 func (session *StratumSession) agentFindWorkerName() {
