21fe9095cd0ad83a2e817bcc9f498b7b9ea04edc
YihaoPeng <yihao.peng@bitmain.com>
2017-08-26 14:05:11 +0800

    StratumSession：实现了简单的mining.subscribe/authorize方法处理

--- StratumSession.go
+++ StratumSession.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"errors"
 	"net"
+	"time"
 
 	"github.com/golang/glog"
 )
@@ -17,6 +18,9 @@ type StratumSession struct {
 	// sessionID 会话ID，也做为矿机挖矿时的 Extranonce1
 	sessionID       uint32
 	sessionIDString string
+
+	fullWorkerName string
+	subaccountName string
 }
 
 // sessionIDManager 会话ID管理器实例
@@ -64,10 +68,11 @@ func (session *StratumSession) Stop() {
 }
 
 func (session *StratumSession) protocolDetect() {
-	magicNumber, err := session.clientReader.Peek(1)
+	magicNumber, err := session.peekWithTimeout(1, 30*time.Second)
 
 	if err != nil {
-		glog.Error("read failed: ", err)
+		glog.Warning("read failed: ", err)
+		session.Stop()
 		return
 	}
 
@@ -80,34 +85,150 @@ func (session *StratumSession) protocolDetect() {
 		session.stratumFindWorkerName()
 
 	} else {
-		glog.Info("Unknown Protocol")
+		glog.Warning("Unknown Protocol")
 		session.Stop()
 	}
 }
 
 func (session *StratumSession) stratumFindWorkerName() {
-	rpcJSON, err := session.clientReader.ReadBytes('\n')
+	e := make(chan error)
 
-	if err != nil {
-		glog.Error("read line failed: ", err)
+	go func() {
+		isSubscribed := false
+		response := new(JSONRPCResponse)
+		running := true
+
+		for running {
+			requestJSON, err := session.clientReader.ReadBytes('\n')
+
+			if err != nil {
+				e <- errors.New("read line failed: " + err.Error())
+				break
+			}
+
+			request, err := NewJSONRPCRequest(requestJSON)
+
+			// ignore the json decode error
+			if err != nil {
+				glog.Warning("JSON decode failed: ", err.Error(), string(requestJSON))
+				continue
+			}
+
+			response.ID = request.ID
+			response.Result = nil
+			response.Error = nil
+
+			switch request.Method {
+			case "mining.subscribe":
+				if isSubscribed {
+					response.Result = nil
+					response.Error = JSONRPCArray{24, "Duplicate Subscribed", nil}
+				} else {
+					isSubscribed = true
+
+					response.Result = "hello"
+					response.Error = nil
+				}
+			case "mining.authorize":
+				if isSubscribed {
+					response.Result = "ok"
+					response.Error = nil
+
+					e <- nil
+					running = false
+
+				} else {
+					response.Result = nil
+					response.Error = JSONRPCArray{25, "Not subscribed", nil}
+				}
+			default:
+				response.Result = nil
+				response.Error = JSONRPCArray{20, "Unknown Method", nil}
+			}
+
+			session.writeJSONResponse(response)
+		}
+
+		close(e)
+	}()
+
+	select {
+	case err := <-e:
+		if err != nil {
+			glog.Warning(err)
+			session.Stop()
+		} else {
+			glog.Warning("auth success")
+		}
+	case <-time.After(90 * time.Second):
+		glog.Warning("FindWorkerName Timeout")
 		session.Stop()
-		return
 	}
-
-	rpcData, err := NewJSONRPCData(rpcJSON)
-
-	if err != nil {
-		glog.Error("JSON decode failed: ", err, rpcJSON)
-		session.Stop()
-		return
-	}
-
-	glog.Info(rpcData.Method)
-	jsonBytes, _ := rpcData.ToJSONBytes()
-	glog.Info(string(jsonBytes))
 }
 
 func (session *StratumSession) agentFindWorkerName() {
 	glog.Error("proxy of BTC Agent Protocol is not implement now!")
 	session.Stop()
+}
+
+func (session *StratumSession) peekWithTimeout(len int, timeout time.Duration) ([]byte, error) {
+	d := make(chan []byte)
+	e := make(chan error)
+
+	go func() {
+		data, err := session.clientReader.Peek(len)
+		if err != nil {
+			e <- err
+		} else {
+			d <- data
+		}
+		close(d)
+		close(e)
+	}()
+
+	select {
+	case data := <-d:
+		return data, nil
+	case err := <-e:
+		return nil, err
+	case <-time.After(timeout):
+		return nil, errors.New("Peek Timeout")
+	}
+}
+
+func (session *StratumSession) readLineWithTimeout(timeout time.Duration) ([]byte, error) {
+	d := make(chan []byte)
+	e := make(chan error)
+
+	go func() {
+		data, err := session.clientReader.ReadBytes('\n')
+		if err != nil {
+			e <- err
+		} else {
+			d <- data
+		}
+		close(d)
+		close(e)
+	}()
+
+	select {
+	case data := <-d:
+		return data, nil
+	case err := <-e:
+		return nil, err
+	case <-time.After(timeout):
+		return nil, errors.New("ReadLine Timeout")
+	}
+}
+
+func (session *StratumSession) writeJSONResponse(jsonData *JSONRPCResponse) (int, error) {
+	bytes, err := jsonData.ToJSONBytes()
+
+	if err != nil {
+		return 0, err
+	}
+
+	defer session.clientWriter.Flush()
+	defer session.clientWriter.WriteByte('\n')
+	return session.clientWriter.Write(bytes)
 }
