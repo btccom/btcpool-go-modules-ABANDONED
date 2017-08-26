0a45a14e5c6f6bee12bc6d4f8db7e2b828496448
YihaoPeng <yihao.peng@bitmain.com>
2017-08-26 14:04:24 +0800

    JSONRPC：分为Request和Response两种数据结构

--- JSONRPC.go
+++ JSONRPC.go
@@ -4,34 +4,63 @@ import (
 	"encoding/json"
 )
 
-// JSONRPCData 是 Stratum 协议里用到的 JSON RPC 的数据结构
-type JSONRPCData struct {
+// JSONRPCRequest JSON RPC 请求的数据结构
+type JSONRPCRequest struct {
 	ID     interface{}   `json:"id"`
 	Method string        `json:"method"`
 	Params []interface{} `json:"params"`
 }
 
-// NewJSONRPCData 解析 JSON RPC 字符串并创建 JSONRPCData 对象
-func NewJSONRPCData(rpcJSON []byte) (*JSONRPCData, error) {
-	rpcData := new(JSONRPCData)
+// JSONRPCResponse JSON RPC 响应的数据结构
+type JSONRPCResponse struct {
+	ID     interface{} `json:"id"`
+	Result interface{} `json:"result"`
+	Error  interface{} `json:"error"`
+}
+
+// JSONRPCResponse JSON RPC 数组
+type JSONRPCArray []interface{}
+
+// NewJSONRPCRequest 解析 JSON RPC 请求字符串并创建 JSONRPCRequest 对象
+func NewJSONRPCRequest(rpcJSON []byte) (*JSONRPCRequest, error) {
+	rpcData := new(JSONRPCRequest)
 
 	err := json.Unmarshal(rpcJSON, &rpcData)
 
 	return rpcData, err
 }
 
-// AddParam 向 JSONRPCData 对象添加一个或多个参数
-func (rpcData *JSONRPCData) AddParam(param ...interface{}) {
+// AddParam 向 JSONRPCRequest 对象添加一个或多个参数
+func (rpcData *JSONRPCRequest) AddParam(param ...interface{}) {
 	rpcData.Params = append(rpcData.Params, param...)
 }
 
-// SetParam 设置 JSONRPCData 对象的参数
-// 传递给 SetParam 的参数列表将按顺序复制到 JSONRPCData.Params 中
-func (rpcData *JSONRPCData) SetParam(param ...interface{}) {
+// SetParam 设置 JSONRPCRequest 对象的参数
+// 传递给 SetParam 的参数列表将按顺序复制到 JSONRPCRequest.Params 中
+func (rpcData *JSONRPCRequest) SetParam(param ...interface{}) {
 	rpcData.Params = param
 }
 
-// ToJSONBytes 将 JSONRPCData 对象转换为 JSON 字节序列
-func (rpcData *JSONRPCData) ToJSONBytes() ([]byte, error) {
+// ToJSONBytes 将 JSONRPCRequest 对象转换为 JSON 字节序列
+func (rpcData *JSONRPCRequest) ToJSONBytes() ([]byte, error) {
+	return json.Marshal(rpcData)
+}
+
+// NewJSONRPCResponse 解析 JSON RPC 响应字符串并创建 JSONRPCResponse 对象
+func NewJSONRPCResponse(rpcJSON []byte) (*JSONRPCResponse, error) {
+	rpcData := new(JSONRPCResponse)
+
+	err := json.Unmarshal(rpcJSON, &rpcData)
+
+	return rpcData, err
+}
+
+// SetResult 设置 JSONRPCResponse 对象的返回结果
+func (rpcData *JSONRPCResponse) SetResult(result interface{}) {
+	rpcData.Result = result
+}
+
+// ToJSONBytes 将 JSONRPCResponse 对象转换为 JSON 字节序列
+func (rpcData *JSONRPCResponse) ToJSONBytes() ([]byte, error) {
 	return json.Marshal(rpcData)
 }
