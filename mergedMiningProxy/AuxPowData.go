a72b7229b95107eec917decba704996e464932ae
YihaoPeng <yihao.peng@bitmain.com>
2018-01-11 21:51:37 +0800

    mergedMiningProxy：可序列化AuxPowData为Hex。

--- mergedMiningProxy/AuxPowData.go
+++ mergedMiningProxy/AuxPowData.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 
 	"./hash"
+	"./merkle"
 )
 
 // AuxMerkleBranch 合并挖矿的 Merkle Branch
@@ -56,8 +57,18 @@ func ParseAuxPowData(dataHex string) (auxPowData *AuxPowData, err error) {
 	// 从字节流中找到 block_hash 以确定 coinbase_txn 的长度
 	index := bytes.Index(data, auxPowData.blockHash[:])
 	if index == -1 {
-		err = errors.New("cannot found blockHash " + auxPowData.blockHash.Hex() + " from AuxPowData " + dataHex)
-		return
+		/* <https://en.bitcoin.it/wiki/Merged_mining_specification#Aux_proof-of-work_block>
+		* Note that the block_hash element is not needed as you have the full parent_block header element
+		* and can calculate the hash from that. The current Namecoin client doesn't check this field for
+		* validity, and as such some AuxPOW blocks have it little-endian, and some have it big-endian.
+		 */
+		auxPowData.blockHash.Reverse()
+		index = bytes.Index(data, auxPowData.blockHash[:])
+		auxPowData.blockHash.Reverse() // Reverse to normal big-endian for other process
+		if index == -1 {
+			err = errors.New("cannot found blockHash " + auxPowData.blockHash.Hex() + " from AuxPowData " + dataHex)
+			return
+		}
 	}
 
 	// index 在数值上等于 coinbase_txn 的长度
@@ -65,14 +76,7 @@ func ParseAuxPowData(dataHex string) (auxPowData *AuxPowData, err error) {
 	copy(auxPowData.coinbaseTxn, data[0:])
 
 	// 跳过 block_hash
-	var blockHash hash.Byte32
-	copy(blockHash[:], data[index:])
 	index += 32
-
-	if blockHash != auxPowData.blockHash {
-		err = errors.New("blockHash " + blockHash.Hex() + " != hash(blockHeader) " + auxPowData.blockHash.Hex())
-		return
-	}
 
 	// coinbaseBranchSize 为变长整数 <https://en.bitcoin.it/wiki/Protocol_documentation#Variable_length_integer> ，
 	// 但是不太可能超过 0xFD。所以假设 coinbaseBranchSize 只有一字节。
@@ -120,4 +124,60 @@ func ParseAuxPowData(dataHex string) (auxPowData *AuxPowData, err error) {
 
 	// 数据合法，解析完成
 	return
+}
+
+// ExpandingBlockchainBranch 将特定币种的MerkleBranch添加到AuxPowData.blockchainBranch
+func (auxPowData *AuxPowData) ExpandingBlockchainBranch(extBranch merkle.MerklePath) {
+	branch := &auxPowData.blockchainBranch
+
+	extBranchLen := uint(len(extBranch))
+	branch.sideMask = branch.sideMask << extBranchLen
+
+	extBranchItems := make([]hash.Byte32, extBranchLen)
+	for i := uint(0); i < extBranchLen; i++ {
+		extBranchItems[i] = extBranch[i].Hash
+		if !extBranch[i].UseFirstInConcatenation {
+			branch.sideMask = branch.sideMask | (uint32(1) << i)
+		}
+	}
+
+	branch.branchs = append(extBranchItems, branch.branchs...)
+}
+
+// ToBytes 把AuxPowData转换为字节流
+func (auxPowData *AuxPowData) ToBytes() (data []byte) {
+
+	// parent coinbase transaction
+	data = append(data, auxPowData.coinbaseTxn...)
+
+	// parent block hash
+	data = append(data, auxPowData.blockHash[:]...)
+
+	// parent coinbase branch
+	data = append(data, byte(len(auxPowData.coinbaseBranch.branchs)))
+	for _, branch := range auxPowData.coinbaseBranch.branchs {
+		data = append(data, branch[:]...)
+	}
+	sideMask := make([]byte, 4)
+	binary.LittleEndian.PutUint32(sideMask, auxPowData.coinbaseBranch.sideMask)
+	data = append(data, sideMask...)
+
+	// merged mining blockchain branch
+	data = append(data, byte(len(auxPowData.blockchainBranch.branchs)))
+	for _, branch := range auxPowData.blockchainBranch.branchs {
+		data = append(data, branch[:]...)
+	}
+	sideMask = make([]byte, 4)
+	binary.LittleEndian.PutUint32(sideMask, auxPowData.blockchainBranch.sideMask)
+	data = append(data, sideMask...)
+
+	// parent block header
+	data = append(data, auxPowData.parentBlock...)
+
+	return
+}
+
+// ToHex 把AuxPowData转换为十六进制字符串
+func (auxPowData *AuxPowData) ToHex() string {
+	return hex.EncodeToString(auxPowData.ToBytes())
 }
